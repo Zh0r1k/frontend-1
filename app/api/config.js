c31d02609165ff8ebbd1631957cdcf9574b8f036
Zh0r1k <149607113+Zh0r1k@users.noreply.github.com>
2024-05-30 20:39:44 +0300

    Update config.js

--- app/api/config.js
+++ app/api/config.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "https://diesel.nomoredomainswork.ru/api";
+export const BASE_URL = "https://eshkere.nomoredomainswork.ru/api";
 
 export const endpoints = {
   games: `${BASE_URL}/games`,
