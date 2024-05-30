cf98142fd21b02fc5d81876834e577632bb2c28e
SweetyOWO <36s@mail.ru>
2024-05-30 17:55:59 +0300

    Update config.js

--- app/api/config.js
+++ app/api/config.js
@@ -1,5 +1,4 @@
-export const BASE_URL = "https://eshkere-front.nomoredomainswork.ru/api";
-
+export const BASE_URL = "https://front-alex.nomoredomainswork.ru/"
 export const endpoints = {
   games: `${BASE_URL}/games`,
   auth: `${BASE_URL}/auth/login`,
